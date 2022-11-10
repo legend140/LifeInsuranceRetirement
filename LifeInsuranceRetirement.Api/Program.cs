79ada3a7f50992604124cd74a1289e5e7062cd28
INTERNAL\RONI43940 <roni.asinjojr@towerswatson.com>
2022-11-10 21:50:41 +0800

    Added gitignore and fix api cors

--- LifeInsuranceRetirement.Api/Program.cs
+++ LifeInsuranceRetirement.Api/Program.cs
@@ -29,6 +29,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 app.UseAuthorization();
 
 app.MapControllers();
